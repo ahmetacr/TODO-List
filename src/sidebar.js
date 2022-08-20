46b878f530d8cd735d8388c882607d4369accd0f
Thomas Anderson <ahmetacar1111@gmail.com>
2022-08-20 15:50:32 -0400

    Delete the comments

--- src/sidebar.js
+++ src/sidebar.js
@@ -12,67 +12,37 @@ export function createSidebar() {
   const calendarIcon = document.createElement('i')
   const upcomingP = document.createElement('p')
   
-  
   const projectSection  = document.createElement('div')
   
   const projectHeader = document.createElement('div')
   const projectP = document.createElement('p')
   const addIcon = document.createElement('i')
   
-  // const projectDiv = document.createElement('div')
-  // const projectName = document.createElement('div')
-  // const nameIcon = document.createElement('i')
-  // const projectNameP = document.createElement('p')
-  // const deleteIcon = document.createElement('i')
-  
-
   sidebar.classList.add('sidebar')
   dateSection.classList.add('dateSection')
   today.classList.add('today')
   todayIcon.classList.add('material-icons')
 
-  upcomingDiv.classList.add('upcoming')
+  upcomingDiv.classList.add('inbox')
   calendarIcon.classList.add('material-icons')
 
   projectSection.classList.add('projectSection')
   projectHeader.classList.add('projectHeader')
   addIcon.classList.add('material-icons')
 
-  // projectDiv.classList.add('project')
-  // projectName.classList.add('projectName')
-  // nameIcon.classList.add('material-icons')
-
-  // deleteIcon.classList.add('material-icons')
-
   todayIcon.textContent = 'today'
   todayP.textContent = 'Today'
 
-  calendarIcon.textContent = 'calendar_month'
-  upcomingP.textContent  = 'Upcoming'
+  calendarIcon.textContent = 'inbox'
+  upcomingP.textContent  = 'Inbox'
 
   projectP.textContent = 'Projects'
   addIcon.textContent = 'add'
-
-  // nameIcon.textContent = 'arrow_right'
-  // projectNameP.textContent = 'Project Name'
-
-  // deleteIcon.textContent = 'delete'
-
-  // projectNameP.id = 'projectName'
-
-  // Append Child
-  // projectName.appendChild(nameIcon)
-  // projectName.appendChild(projectNameP)
-
-  // projectDiv.appendChild(projectName)
-  // projectDiv.appendChild(deleteIcon)
 
   projectHeader.appendChild(projectP)
   projectHeader.appendChild(addIcon)
 
   projectSection.appendChild(projectHeader)
-  // projectSection.appendChild(projectDiv)
-
 
   today.appendChild(todayIcon)
   today.appendChild(todayP)
@@ -80,8 +50,8 @@ export function createSidebar() {
   upcomingDiv.appendChild(calendarIcon)
   upcomingDiv.appendChild(upcomingP)
 
-  dateSection.appendChild(today)
   dateSection.appendChild(upcomingDiv)
+  dateSection.appendChild(today)
 
 
   sidebar.appendChild(dateSection)
