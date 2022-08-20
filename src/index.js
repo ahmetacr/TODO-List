8d8a2b6b838f2ae59e8ba78528a059a84cdaf161
Thomas Anderson <ahmetacar1111@gmail.com>
2022-08-20 15:59:49 -0400

    Fix bugs and work on workflow:

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import './style.css';
 import { compareAsc, format } from 'date-fns'
 import { deleteTODO } from './delete_todo.js';
 import { createHeader } from './header.js'
-import { addEvent } from './addEvent';
-import { addProject } from './addProject';
+import { addEvent as taskForm } from './taskForm';
+import { addProject as projectForm } from './projectForm';
 import { createSidebar } from './sidebar.js';
+
+/**
+ * Fix those bugs:
+ * 
+ */   
+
 
 (function() {
   // Define Icons
@@ -12,6 +18,11 @@ import { createSidebar } from './sidebar.js';
   iconLink.setAttribute('rel','stylesheet')
   iconLink.setAttribute('href','https://fonts.googleapis.com/icon?family=Material+Icons')
   document.head.appendChild(iconLink)
+
+  // Add a hidden background
+  const hiddenBG = document.createElement('div');
+  hiddenBG.classList.add('hiddenBG')
+  document.body.appendChild(hiddenBG)
 
   // Add header 
   const header = createHeader();
@@ -29,17 +40,13 @@ import { createSidebar } from './sidebar.js';
   mainPage.classList.add('mainPage')
   container.appendChild(mainPage)
 
-
   // Event Box
-  const taskBox = addEvent();
+  const taskBox = taskForm();
   document.body.appendChild(taskBox.taskForm);
 
   // Project Box
-  const projectBox = addProject();
+  const projectBox = projectForm();
   document.body.appendChild(projectBox.projectForm)
-
-
-
 
   // Project Class to create a new project
   class Project {
@@ -55,10 +62,9 @@ import { createSidebar } from './sidebar.js';
   class Task extends Project {
     constructor(title,description,due,priority) {
       super(title,description,due,priority)
+      this.toProject = this.toProject
     }
   }
-
-
 
   const Create = (function() {
 
@@ -68,11 +74,11 @@ import { createSidebar } from './sidebar.js';
     }
 
     const _createProjectBox = () => {
-      DisplayInfo.displayProjectBox();
+      DisplayInfo.displayProjectForm();
     }
 
     const setProjectInfo = (newProject) => {
-      newProject.title = document.getElementById('projectTitleInput').value
+      newProject.title = document.getElementById('projectTitleInput').value.replaceAll(' ','-')
       newProject.description = document.getElementById('projectDescriptionInput').value
       newProject.due = document.getElementById('projectDateInput').value
     }
@@ -92,7 +98,7 @@ import { createSidebar } from './sidebar.js';
       project.id = title
 
       projectIcon.textContent = 'arrow_right'
-      projectNameP.textContent = title
+      projectNameP.textContent = title.replaceAll('-',' ')
       projectDeleteIcon.textContent = 'delete'
 
       projectName.appendChild(projectIcon)
@@ -117,9 +123,9 @@ import { createSidebar } from './sidebar.js';
       projectDescription.id = 'projectDescription'
       projectDate.id = 'projectDate'
 
-      projectName.textContent = project.title
+      projectName.textContent = project.title.replaceAll('-',' ')
       projectDescription.textContent = project.description
-      projectDate.textContent = `Project is Due: ${project.due}` 
+      projectDate.textContent = `Due: ${project.due}` 
 
       projectDisplayBox.appendChild(projectName)
       projectDisplayBox.appendChild(projectDescription)
@@ -142,15 +148,38 @@ import { createSidebar } from './sidebar.js';
       DisplayInfo.displayProject(newProject.title)
 
       // Disappear the project box from the sidebar
-      DisplayInfo.disappearProjectBox()
+      DisplayInfo.disappearProjectForm()
+
+      // Add the project on the main page
+      const projectDiv = Create.createProjectDetails(newProject)
+      projectDiv.style.display = 'none'
+      document.querySelector('.mainPage').prepend(projectDiv) 
 
       // Display the CLICKED project object on the main page!
-      document.querySelector(`#${newProject.title}`).addEventListener('click',(event) => {
-        const activeProject = document.querySelector('.active')
-        if(activeProject) activeProject.classList.remove('active');
-        DisplayInfo.displayProjectInfo(newProject)
-        event.currentTarget.classList.add('active')
+      document.querySelector(`.projectSection #${newProject.title}`).addEventListener('click',(event) => {
+
+        // If the clicked one is active do nothing!
+        if (event.currentTarget.classList.contains('active')) {
+          console.log('This project is already active!')
+          console.log(event.currentTarget.classList)
+        } else {
+          DisplayInfo.displayProjectInfo(projectDiv)
+          console.log(projectDiv)
+          projectDiv.classList.add('activeProject')
+          document.querySelectorAll('.task').forEach(task => task.style.display = 'none')
+          document.querySelectorAll(`.mainPage div[data-project="${newProject.title}"]`).forEach(task => task.style.display = 'flex')
+
+          let activeProject = document.querySelector('.active')
+          if (activeProject) { 
+            activeProject.classList.remove('active')
+            document.querySelector(`.mainPage > #${activeProject.id}`).style.display = 'none'
+            document.querySelector(`.mainPage > #${activeProject.id}`).classList.remove('activeProject')
+          }
+          event.currentTarget.classList.add('active')        
+        }
       })
+      // Add the project name to the Select options of task creation
+      DisplayInfo.addProjecttoTaskForm(newProject.title)
 
       // Delete Project
       document.querySelector(`#${newProject.title} > i`).addEventListener('click',_deleteProject)
@@ -161,11 +190,16 @@ import { createSidebar } from './sidebar.js';
 
     const _deleteProject = (event) => {
       const removedProject = event.path[1]
+      document.getElementById('select-project').removeChild(document.querySelector(`option[value="${removedProject.id}"`))
+
       const projectSection = document.querySelector('.projectSection')
-      const projectOnTheScreen = document.querySelector('.mainPage > div')
+      const projectOnTheScreen = document.querySelector(`.mainPage > #${removedProject.id}`)
       if (projectOnTheScreen !== null) {
         if (projectOnTheScreen.id == event.path[1].id) {
+          // Remove the project on the screen
           document.querySelector('.mainPage').removeChild(projectOnTheScreen)
+          // delete the realted tasks
+          document.querySelectorAll(`.mainPage div[data-project="${projectOnTheScreen.id}"]`).forEach(task => document.querySelector('.mainPage').removeChild(task))
         }
       }
       projectSection.removeChild(removedProject)
@@ -175,7 +209,7 @@ import { createSidebar } from './sidebar.js';
 
     const _cancelProject = () => {
       projectBox.projectForm.reset()
-      DisplayInfo.disappearProjectBox()
+      DisplayInfo.disappearProjectForm()
     }
 
     const _listenProject = () => {
@@ -190,16 +224,18 @@ import { createSidebar } from './sidebar.js';
     }
 
     const _createTaskBox = () => {
-      DisplayInfo.displayTaskBox();
+      DisplayInfo.displayTaskForm();
     }
 
     const setTaskInfo = (newTask) => {
+      // addEvent'ten donen itemlere de direkt ekleyebiliriz document.getEvent'dense
       newTask.title = document.getElementById('taskTitleInput').value
       newTask.description = document.getElementById('taskDescriptionInput').value
       newTask.due = document.getElementById('taskDateInput').value
+      newTask.toProject = document.getElementById('select-project').value
     }
 
-    const createTaskDiv = (title,due) => {
+    const createTaskDiv = (title,due,toProject) => {
       const task = document.createElement('div')
       
       const taskName = document.createElement('div')
@@ -215,6 +251,8 @@ import { createSidebar } from './sidebar.js';
       task.classList.add('task')
       taskName.classList.add('taskName')
       taskDueDate.classList.add('taskDueDate')
+
+      task.setAttribute('data-project',toProject)
 
       task.id = `${title}`
       taskTitleP.id = 'taskTitle'
@@ -243,6 +281,8 @@ import { createSidebar } from './sidebar.js';
     }
 
     const _manageTask = (event) => {
+      event.preventDefault()
+      
       // Create the task
       const newTask = createTask()
 
@@ -250,16 +290,15 @@ import { createSidebar } from './sidebar.js';
       setTaskInfo(newTask)
 
       // Display the task object on the page!
-      DisplayInfo.displayTask(newTask.title,newTask.due)
+      DisplayInfo.displayTask(newTask.title,newTask.due,newTask.toProject)
 
       // Disappear the task box
-      DisplayInfo.disappearTaskBox()
+      DisplayInfo.disappearTaskForm()
 
       // delete task
       document.getElementById(`${newTask.title}`).addEventListener('change',_deleteTask)
 
       // Prevent from reloading page!
-      event.preventDefault();
       event.target.reset()
     }
 
@@ -271,7 +310,33 @@ import { createSidebar } from './sidebar.js';
 
     const _cancelTask = () => {
       taskBox.taskForm.reset();
-      DisplayInfo.disappearTaskBox()
+      DisplayInfo.disappearTaskForm()
+    }
+
+    const createMainPage = () => {
+      DisplayInfo.displayMainPage()
+    }
+
+    const manageToday = () => {
+      
+    }
+
+    const createTodayDiv = () => {
+      const todayDiv = document.createElement('div')
+      const todayHeader = document.createElement('h1')
+
+      todayDiv.classList.add('todayDiv')
+      todayHeader.id = 'todayHeader'
+
+      todayHeader.textContent = 'Today'
+
+      todayDiv.appendChild(todayHeader)
+
+      return todayDiv
+    }
+
+    const _listenMainPage = () => {
+      document.getElementById('home').addEventListener('click', createMainPage)
     }
 
     const _listenTask = () => {
@@ -283,6 +348,7 @@ import { createSidebar } from './sidebar.js';
     const _listen =  (function() {
       _listenProject()
       _listenTask()
+      _listenMainPage()
     })()
 
     return {
@@ -292,20 +358,24 @@ import { createSidebar } from './sidebar.js';
       createProject,
       createProjectDiv,
       setProjectInfo,
-      createProjectDetails
+      createProjectDetails,
+      createTodayDiv
     }
 
   })();
 
-
   const DisplayInfo = (function() {
 
-    const displayProjectBox = () => {
-      projectBox.projectForm.style.display = 'block'
+    const displayProjectForm = () => {
+      projectBox.projectForm.style.cssText = 'display:block;'
+      projectBox.projectForm.classList.add('fade-in')
+      hiddenBG.style.cssText = 'z-index:1; opacity:0.5'
     }
 
-    const disappearProjectBox = () => {
-      projectBox.projectForm.style.display = 'none'
+    const disappearProjectForm = () => {
+      projectBox.projectForm.style.cssText = 'display:none;'
+      projectBox.projectForm.classList.remove('fade-in')
+      hiddenBG.style.cssText = 'z-index:-1; opacity:0'
     }
 
     const displayProject = (title) => {
@@ -313,27 +383,70 @@ import { createSidebar } from './sidebar.js';
       document.querySelector('.projectSection').appendChild(project)
     }
 
-    const displayProjectInfo = (project) => {
-      if (document.querySelector('.mainPage').innerHTML) clearMainPage()
-      const projectDiv = Create.createProjectDetails(project)
-      document.querySelector('.mainPage').appendChild(projectDiv)
+    const displayProjectInfo = (projectDiv) => {
+      projectDiv.style.display = 'flex'
     }
 
-    const displayTaskBox = () => {
+    const displayTaskForm = () => {
       taskBox.taskForm.style.display = 'block'
+      taskBox.taskForm.classList.add('fade-in')
+      hiddenBG.style.cssText = 'z-index:1; opacity:0.5'
     }
 
-    const disappearTaskBox = () => {
+    const disappearTaskForm = () => {
       taskBox.taskForm.style.display = 'none'
+      taskBox.taskForm.classList.remove('fade-in')
+      hiddenBG.style.cssText = 'z-index:-1; opacity:0'
     }
 
-    const displayTask = (title,due) => {
-      const task = Create.createTaskDiv(title,due)  
+    const displayTask = (title,due,toProject) => {
+      //  eger ekranda bir proje varsa ve bu projeye task ekleniyorsa o halde display flex olarak eklenmeli
+      const task = Create.createTaskDiv(title,due,toProject) 
       mainPage.appendChild(task)
+      const tasksOfProject = (document.querySelectorAll(`.mainPage div[data-project="${toProject}"]`))
+      tasksOfProject.forEach(task => task.style.display = 'none')
+      // Bu alttaki if statemetlarda sadece ilk taski eklerken ekranda proje var mi ona bakiyorum
+      // su anda ekranda bir proje var mi ve varsa eger bu projenin id'siyle benim taskimin data-project'i uyuyor mu 
+      if (document.querySelector('.mainPage > div')) {
+        document.querySelectorAll('.projectDisplayBox').forEach(project => {
+          if (project.classList.contains('activeProject')) {
+            tasksOfProject.forEach(task => {
+              if (task.getAttribute('data-project') == project.id) {
+                console.log('calisti')
+                task.style.display = 'flex'
+              }
+              else {
+                task.style.display = 'none'
+              }
+            })
+          }
+        })
+      }
     }
 
     const manageTasksDisplay = (display) => {
       document.querySelectorAll('.task').forEach(task => task.style.display = display)
+    }
+
+    const manageProjectDisplay = (display) => {
+      if (document.querySelector('.projectDisplayBox')) document.querySelector('.projectDisplayBox').style.display = display
+    }
+
+    const displayMainPage = () => {
+      manageTasksDisplay('flex')
+      manageProjectDisplay('none')
+    }
+
+    const displayTodayDiv = () => {
+      const todayDiv = Create.createTodayDiv()
+      todayDiv.style.display = 'block'
+    }
+
+    const addProjecttoTaskForm = (projectName) => {
+      const projectOption = document.createElement('option')
+      projectOption.textContent = projectName
+      projectOption.value = projectName
+      document.querySelector('#select-project').appendChild(projectOption)
     }
 
     const clearMainPage = () => {
@@ -341,24 +454,15 @@ import { createSidebar } from './sidebar.js';
     }
 
     return {
-      displayTaskBox,
-      disappearTaskBox,
+      displayTaskForm: displayTaskForm,
+      disappearTaskForm: disappearTaskForm,
       displayTask,
-      displayProjectBox,
-      disappearProjectBox,
+      displayProjectForm: displayProjectForm,
+      disappearProjectForm: disappearProjectForm,
       displayProject,
-      displayProjectInfo
+      displayProjectInfo,
+      displayMainPage,
+      addProjecttoTaskForm
     }
   })()
 })()
-
-
-
-
-
-
-
-
-
-
-
